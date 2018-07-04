74444a71cd366df6b952421d2d34a0ee0dc3a78e
Quentin Dufour <quentin@dufour.io>
2018-07-04 21:04:58 +0200

    Working sound in Scheme

--- gstreamer-test/main.scm
+++ gstreamer-test/main.scm
@@ -1,21 +1,34 @@
 (load-shared-object "libgstreamer-1.0.so")
 
-(define gst-init (foreign-procedure "gst_init" (ptr void*) void))
-(define g-main-loop-new (foreign-procedure "g_main_loop_new" (void* int) void*))
+(define (gst-state->int symbol) 
+  (case symbol
+    ((void-pending) 0)
+    ((null)         1)
+    ((ready)        2)
+    ((paused)       3)
+    ((playing)      4)))
+
+(define g-main-loop-new (foreign-procedure "g_main_loop_new" (void* boolean) void*))
 (define g-object-set (foreign-procedure "g_object_set" (void* string string void*) void))
+
+(define gst-init (foreign-procedure "gst_init" (ptr void*) void))
 (define gst-element-factory-make (foreign-procedure "gst_element_factory_make" (string string) void*))
 (define gst-pipeline-get-bus (foreign-procedure "gst_pipeline_get_bus" (void*) void*))
-(define gst-bus-add-watch (foreign-procedure "gst_bus_add_watch" (void* void* void*) void*)) ; @TODO
+(define gst-bus-add-watch (foreign-procedure "gst_bus_add_watch" (void* void* void*) void))
 (define gst-object-unref (foreign-procedure "gst_object_unref" (void*) void))
+(define gst-element-set-state (foreign-procedure "gst_element_set_state" (void* int) void))
 
 (gst-init 0 0)
 (let*
-  ([loop (g-main-loop-new 0 0)]
+  ([loop (g-main-loop-new 0 #f)]
    [play (gst-element-factory-make "playbin" "play")]
-   [bus  (gst-pipeline-get-bus play)])
-  (display bus)
+   [bus  (gst-pipeline-get-bus play)]
+   [control (foreign-callable (lambda (bus2 msg loop2) #t) (void* void* void*) boolean)]
+   [control2 (foreign-callable-entry-point control)])
   (g-object-set play "uri" "file:////home/quentin/Musique/firewatch/Camp Approach-2120717428.mp3" 0)
-  ;(gst-bus-add-watch bus ?? loop)
+  (gst-bus-add-watch bus control2 loop)
   (gst-object-unref bus)
+  (gst-element-set-state play (gst-state->int 'playing))
+  (display "done")
 )
   
